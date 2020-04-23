1adfe2d83ee73d0544624b69b4c01e9002763e7e
iamsnj <skdakshana18@gmail.com>
2020-04-23 11:32:43 +0530

    admin page created and login page improved

---
+++ app/forms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 22:22:08 2020
+
+@author: sanjay
+"""
+
+
+from wtforms import Form, StringField, PasswordField, SubmitField, RadioField
+from wtforms.fields.html5 import EmailField
+from wtforms import validators
+
+class Register(Form):
+    username = StringField('Username', [validators.DataRequired()])
+    email = EmailField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired()])                                               
+    submit = SubmitField('Sign Up')
+
+class Sign_In(Form):
+    whichUser = RadioField('Login As', choices = [('A','Admin'),('T','TA')], default='A')  
+    userId = StringField('UserId', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
+    submit = SubmitField('Submit')
+
+class change_password(Form):
+    password = PasswordField('Password', [validators.DataRequired()])
+    confirm = PasswordField('Confirm Your Password', [
+        validators.DataRequired(),
+        validators.EqualTo('password', message='Passwords must match.')])
