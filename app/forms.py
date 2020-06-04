c4cc6c5af000d731e69600add4a6e33a205ecfdf
iamsnj <skdakshana18@gmail.com>
2020-06-04 10:36:23 +0530

    q

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,6 @@ Created on Fri Apr 10 22:22:08 2020
 @author: sanjay
 """
 
-
 from wtforms import Form, StringField, PasswordField, SubmitField, RadioField
 from wtforms.fields.html5 import EmailField
 from wtforms import validators
@@ -14,11 +13,11 @@ from wtforms import validators
 class Register(Form):
     username = StringField('Username', [validators.DataRequired()])
     email = EmailField('Email', [validators.DataRequired(), validators.Email()])
-    password = PasswordField('Password', [validators.DataRequired()])                                               
+    password = PasswordField('Password', [validators.DataRequired()])
     submit = SubmitField('Sign Up')
 
 class Sign_In(Form):
-    whichUser = RadioField('Login As', choices = [('A','Admin'),('T','TA')], default='A')  
+    whichUser = RadioField('Login As', choices = [('A','Admin'),('T','TA')], default='A')
     userId = StringField('UserId', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
     submit = SubmitField('Log In')
